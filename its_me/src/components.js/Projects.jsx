5aedefc509f7b84dfe7aa713c5c3ccdb0bc4b9c4
shubhammm14 <shubhamshaw905@gmail.com>
2024-02-05 02:20:46 +0530

    wagee commit

--- its_me/src/components.js/Projects.jsx
+++ its_me/src/components.js/Projects.jsx
@@ -11,15 +11,15 @@ const Projects = () => {
           <div className='flex flex-col'>
             <div className='flex flex-row m-10  '>
               <div className='w-[30vw]  m-10'>
-                <Card projectName='Cuurency Exchanger' />
+                <a href='https://shubhammm14.github.io/Currency-Exchanger/'><Card projectName='Currency Exchanger' /></a>
               </div>
               <div className='w-[30vw] m-10'>
-                <Card projectName='My-TodoApp' />
+               <a href='https://shubhammm14.github.io/My-TodoApp/'> <Card projectName='My-TodoApp' /></a>
               </div>
             </div>
             <div className='flex flex-row m-10'>
               <div className='w-[30vw] m-10 '>
-                <Card projectName='Bg-Changer' />
+                <a href='https://shubhammm14.github.io/Bg-Changer/'><Card projectName='Bg-Changer' /></a>
               </div>
               <div className='w-[30vw] m-10 '>
                 <Card projectName='RandomColor-Painter' />
@@ -32,18 +32,18 @@ const Projects = () => {
           <div className='flex flex-col'>
             <div className='flex flex-row m-10 '>
               <div className='w-[30vw] m-10'>
-                <Card projectName='Instagram-Backend Api Clone' />
+               <a href='https://github.com/Shubhammm14/PersonalProjects2-InstagramWeb-Backend'> <Card projectName='Instagram-Backend Api Clone' /></a>
               </div>
               <div className='w-[30vw] m-10'>
-                <Card projectName='Whatsapp-Backend Api Clone' />
+                <a href='https://github.com/Shubhammm14/PersonalProject3-whatsappWebBackend'><Card projectName='Whatsapp-Backend Api Clone' /></a>
               </div>
             </div>
             <div className='flex flex-row m-10'>
               <div className='w-[30vw] m-10 '>
-                <Card projectName='Email-Sender Using SMVT Protocol' />
+                <a href='https://github.com/Shubhammm14/Api1/tree/main/Simple%20Email%20Sender%20Using%20SMTP'><Card projectName='Email-Sender Using SMVT Protocol' /></a>
               </div>
               <div className='w-[30vw] m-10 '>
-                <Card projectName='Book Management Api' />
+                <a href='https://github.com/Shubhammm14/SpringBoot-REACT-FullStack/tree/app-0'><Card projectName='Student Management Backend Api' /></a>
               </div>
             </div>
           </div>
